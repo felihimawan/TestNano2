77f91566a12d350d6befce11621f3ae20dfcc26f
Felicia Himawan <ffeliciah.00@gmail.com>
2024-05-15 21:08:06 +0700

    Initial Commit

---
+++ TestNano2/TestNano2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestNano2App.swift
+//  TestNano2
+//
+//  Created by Felicia Himawan on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TestNano2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
